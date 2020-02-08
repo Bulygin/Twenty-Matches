b44b8a73baa8a731a6b41b8368859e9f10a7b5b0
Bulygin <jbulygin@yandex.ru>
2020-02-08 12:43:47 +0300

    final

---
+++ src/com/company/service/MessageService.java
@@ -0,0 +1,48 @@
+package com.company.service;
+
+class MessageService {
+
+  private static final String BEGIN_TEXT =
+      "Rules:\n" + "20 matches on thetable.\n" + "You take 1, 2 or 3 matches.\n"
+          + "If you take last match, you lose.\n\n";
+
+  //Start game, write rules
+  static void start() {
+    System.out.println(BEGIN_TEXT);
+  }
+
+  //Every turn write the remaining matches '|'
+  static void nextTurn(int matches, boolean isRobotTurn) {
+    for (int i = 0; i < 20; i++) {
+      if (i < matches) {
+        System.out.print('|');
+      }
+    }
+    System.out.print('\n');
+  }
+
+  //Writes how many matches the robot took
+  static void robotTurn(int turn) {
+    System.out.printf("Robot take: %d matches\n", turn);
+  }
+
+  //Write human turn
+  static void humanTurn() {
+    System.out.print("You took: ");
+  }
+
+  //Write error, if user enter uncommon number
+  static void error() {
+    System.out.println("Incorrect value, enter 1, 2 or 3.\n");
+  }
+
+  //Write game results
+  static void gameOver(boolean isRobotTurn) {
+    if (isRobotTurn) {
+      System.out.println("You lose!");
+    } else {
+      System.out.println("You win!");
+    }
+  }
+
+}
